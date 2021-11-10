d902db98345f8a2f7382954e7a4628b732164fd0
calebbeswick <beswica@ssfs.org>
2021-11-10 10:59:15 -0500

    strings

--- strings_two.py
+++ strings_two.py
@@ -1,11 +1,26 @@
 def four_letters(phrase):
-    words = phrase.split()
-    list_two = []
-    for word in words:
-        if len(word) == 4:
-            list_two
+    split_list = phrase.split()
+    for x in range(len(split_list)):
+        if len(split_list[x]) == 4:
+            split_list[x] = "#$%@"
+    list_two = " ".join(split_list)
+    return list_two
+
+
 
 
 
 def bubble_sort(words):
-    pass
+    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'j', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'y', 'z']
+    alphabetical = False
+    while alphabetical != True:
+        for x in range(len(words)):
+            for i in range(len(words)-1)
+                if words[i] > words[i+1]
+                temp1 = words[i]
+                temp2 = words[i + 1]
+                words[i] = temp2
+                words[i+1] = temp1
+if __name__ == '__main__':
+    four_letters("have a nice day")
+    bubble_sort()
