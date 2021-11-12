b5a98bdab2cc892dcbb309c9118d68df3aa3c3e1
calebbeswick <beswica@ssfs.org>
2021-11-12 14:27:04 -0500

    unit 7 project

--- assignment_seven.py
+++ assignment_seven.py
@@ -9,6 +9,12 @@ list_of_words = text.readlines()
 text.close()
 
 
+
+alpha = open("Alphabet", "r")
+list_of_letters = alpha.readlines()
+text.close()
+
+
 # words is if it is random or three words
 #length_of_password is how many characters in the password
 #type_of_pas
@@ -17,12 +23,33 @@ text.close()
 def random_word():
     word = []
     rand_word1 = random.choice(list_of_words)
-    rand_word2 = random.choice(list_of_words)
-    rand_word3 = random.choice(list_of_words)
-    word = str(rand_word1) + str(rand_word3) + str(rand_word2)
-    print(word)
+    return rand_word1
+
+
+def random_password():
+    characters = int(input("Please enter the amount of characters of your password "))
+    case = input("Would you like capital letters? (y/n) ")
+    numbers = input("Would you like numbers? (y/n) ")
+    symbols = input("Would you like symbols? (y/n) ")
+    empty_list = []
+    for letters in range(characters):
+        num = random.randrange(0, 1)
+        num2 = random.randrange(0, 3)
+        if case == "y" and num == 1:
+            if numbers == "y" and num2 == 3:
+                character = random.randint(0, 9)
+            elif num2 == 0:
+                character = random.choice(list_of_letters)
+                character = character.capitalize()
+            elif num2 == 1:
+                character = random.choice(list_of_letters)
+        else:
+            character = random.choice(list_of_letters)
+        empty_list.append(character)
+    print(empty_list)
+
+
 
 random_word()
-
-
+random_password()
 
